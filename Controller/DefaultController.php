cfd0630f09614c13f95ca3adfe2e7108cb6d8748
Gaston Caldeiro <chugas488@gmail.com>
2013-04-12 16:03:25 -0300

    Redirijiendo url /admin to /admin/login

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,6 +9,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-      return new RedirectResponse($this->container->get('router')->generate('fos_user_security_login'));
+      $user = $this->get('security.context')->getToken()->getUser();
+      if($user){
+        return new RedirectResponse($this->container->get('router')->generate('sonata_admin_dashboard'));        
+      }else{
+        return new RedirectResponse($this->container->get('router')->generate('fos_user_security_login'));        
+      }
     }
 }
